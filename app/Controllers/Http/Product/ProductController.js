e58a11b0ca7dfead787330780f50787f453d4b2f
Victor Jonah <victorjonah199@gmail.com>
2022-01-12 14:50:14 +0100

    feat: create product sub category

--- app/Controllers/Http/Product/ProductController.js
+++ app/Controllers/Http/Product/ProductController.js
@@ -1,12 +1,16 @@
 'use strict';
 const CreateProductFeature = use('App/Features/Product/CreateProductFeature');
 const CreateProductCategoryFeature = use('App/Features/ProductCategory/CreateProductCategoryFeature');
+const CreateProductSubCategoryFeature = use('App/Features/ProductSubCategory/CreateProductSubCategoryFeature');
 const ReadProductFeature = use('App/Features/Product/ReadProductFeature');
 const ReadProductCategoryFeature = use('App/Features/ProductCategory/ReadProductCategoryFeature');
+const ReadProductSubCategoryFeature = use('App/Features/ProductSubCategory/ReadProductSubCategoryFeature');
 const UpdateProductFeature = use('App/Features/Product/UpdateProductFeature');
 const UpdateProductCategoryFeature = use('App/Features/ProductCategory/UpdateProductCategoryFeature');
+const UpdateProductSubCategoryFeature = use('App/Features/ProductSubCategory/UpdateProductSubCategoryFeature');
 const DeleteProductFeature = use('App/Features/Product/DeleteProductFeature');
 const DeleteProductCategoryFeature = use('App/Features/ProductCategory/DeleteProductCategoryFeature');
+const DeleteProductSubCategoryFeature = use('App/Features/ProductSubCategory/DeleteProductSubCategoryFeature');
 
 class ProductController {
   async createProduct({ request, response, auth }) {
@@ -17,12 +21,20 @@ class ProductController {
     return new CreateProductCategoryFeature(request, response, auth).handle();
   }
 
+  async createProductSubCategory({ request, response, auth }) {
+    return new CreateProductSubCategoryFeature(request, response, auth).handle();
+  }
+
   async readProduct({ request, response, params }) {
     return new ReadProductFeature(request, response, params).handle();
   }
 
   async readProductCategory({ request, response, params: { product_category_id } }) {
     return new ReadProductCategoryFeature(request, response, product_category_id).handle();
+  }
+
+  async readProductSubCategory({ request, response, params: { product_sub_category_id } }) {
+    return new ReadProductSubCategoryFeature(request, response, product_sub_category_id).handle();
   }
 
   async updateProduct({ request, response, params }) {
@@ -33,12 +45,20 @@ class ProductController {
     return new UpdateProductCategoryFeature(request, response, product_category_id).handle();
   }
 
+  async updateProductSubCategory({ request, response, params: { product_sub_category_id } }) {
+    return new UpdateProductSubCategoryFeature(request, response, product_sub_category_id).handle();
+  }
+
   async deleteProduct({ request, response, params }) {
     return new DeleteProductFeature(request, response, params).handle();
   }
 
   async deleteProductCategory({ request, response, params: { product_category_id } }) {
     return new DeleteProductCategoryFeature(request, response, product_category_id).handle();
+  }
+
+  async deleteProductSubCategory({ request, response, params: { product_sub_category_id } }) {
+    return new DeleteProductSubCategoryFeature(request, response, product_sub_category_id).handle();
   }
 }
 
---
+++ app/Features/ProductSubCategory/CreateProductSubCategoryFeature.js
@@ -0,0 +1,36 @@
+'use strict';
+const ProductSubCategory = use('App/Models/ProductSubCategory');
+const ProductCategory = use('App/Models/ProductCategory');
+const NotFoundException = use('App/Exceptions/NotFoundException');
+
+class CreateProductSubCategoryFeature {
+  constructor(request, response, auth) {
+    this.request = request;
+    this.response = response;
+    this.auth = auth;
+  }
+
+  async handle() {
+    const { name, product_category_id, status } = this.request.all();
+
+    const productCategory = await ProductCategory.findBy('id', product_category_id);
+    if (!productCategory) {
+      throw new NotFoundException('Product Category');
+    }
+
+    const productSubCategory = new ProductSubCategory();
+    productSubCategory.name = name;
+    productSubCategory.status = Boolean(status);
+    productSubCategory.product_category_id = product_category_id;
+
+    productSubCategory.save();
+
+    return this.response.status(200).send({
+      message: 'Product Category Create Successful',
+      status: 'Success',
+      status_code: 200,
+      result: productSubCategory,
+    });
+  }
+}
+module.exports = CreateProductSubCategoryFeature;
