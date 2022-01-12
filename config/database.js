45aa5c78214b6b61baf989b494351c4fc07a4590
Victor Jonah <victorjonah199@gmail.com>
2022-01-12 16:17:18 +0100

    chore: clearDB config

--- config/database.js
+++ config/database.js
@@ -1,10 +1,12 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
+const Env = use('Env');
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
-const Helpers = use('Helpers')
+const Helpers = use('Helpers');
+const Url = require('url-parse');
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'));
 
 module.exports = {
   /*
@@ -50,12 +52,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
-      charset: 'utf8mb4',
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1)),
     },
     pool: {
       min: 8,
@@ -84,4 +85,4 @@ module.exports = {
       database: Env.get('DB_DATABASE', 'adonis'),
     },
   },
-}
+};
